311c452175aa59012d52446d979e87e0ec972346
Jennifer0914H <jennifer0914H@outlook.com>
2026-02-25 19:06:06 +0300

    update intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
               duration: 0.2,
             }}
           >
-            <Image
+            {/* <Image
               src={profile.avatar}
               alt="Ketan portrait"
               width="192"
@@ -39,10 +39,10 @@ export default function Intro() {
               quality="95"
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-            />
+            /> */}
           </motion.div>
 
-          <motion.span
+          {/* <motion.span
             className="absolute bottom-0 right-0 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -54,7 +54,7 @@ export default function Intro() {
             }}
           >
             👋
-          </motion.span>
+          </motion.span> */}
         </div>
       </div>
 
