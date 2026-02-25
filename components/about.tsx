d074ac8f1122b131634a931b0d307970366da635
Jennifer0914H <jennifer0914H@outlook.com>
2026-02-25 16:28:53 +0300

    update about

--- components/about.tsx
+++ components/about.tsx
@@ -19,29 +19,21 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a postgraduate degree in{" "}
-        <span className="font-medium">Computer Science</span>, Upon graduation,
-        I embarked on a journey to become a{" "}
-        <span className="font-medium">full-stack web developer</span>. What
-        drives me most is the thrill of untangling a complex problem and
-        crafting an effective solution. My core stack is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, MongoDB, and React Native
-        </span>{" "}
-        allowing me to build dynamic and engaging applications across the{" "}
-        <span className="italic">web and mobile platforms</span>. I am also
-        familiar with{" "}
-        <span className="font-medium">
-          TypeScript, electronJS, Python, Flask and fastapi
-        </span>
-        . I am always looking to learn new technologies. I am currently looking
-        for a <span className="font-medium">full-time position</span> as a
-        software developer.
+        <span className="font-medium">Senior Full Stack Engineer</span> with 5+ years of experience 
+        <br></br>
+        building and scaling SaaS platforms using <span className="font-medium"> React, Redux, Node.js, and MongoDB</span>. 
+        <br></br>
+        Strong background in 
+        <span className="font-medium"> performance optimization, database design</span> and
+        <br></br>
+        <span className="font-medium"> scalable API architecture</span>.
+        Experienced working in product-driven, fast-growing tech companies.
+        Comfortable collaborating in international English-speaking teams and contributing to system design decisions.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies and anime series, or taking walks for a
+        badminton, watching movies, listening to music or <br></br>taking walks for a
         breath of fresh air.
       </p>
     </motion.section>
--- components/intro.tsx
+++ components/intro.tsx
@@ -116,7 +116,7 @@ export default function Intro() {
           <BsArrowDown className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        <a
+        {/* <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href={profile.resumeLink}
           target="_blank"
@@ -125,15 +125,15 @@ export default function Intro() {
         >
           Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
+        </a> */}
 
-        <a
+        {/* <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href={profile.linkedInLink}
           target="_blank"
         >
           <BsLinkedin />
-        </a>
+        </a> */}
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
